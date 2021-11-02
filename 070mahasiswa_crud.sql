5740de762421b18ef28f2c24d5f59221a52b4a79
DevianandaYunitasari <92282787+DevianandaYunitasari@users.noreply.github.com>
2021-11-02 09:38:05 +0700

    Add files via upload

---
+++ 070mahasiswa_crud.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 01 Nov 2021 pada 15.36
+-- Versi server: 10.4.21-MariaDB
+-- Versi PHP: 7.3.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `070mahasiswa_crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_mhs`
+--
+
+CREATE TABLE `tbl_mhs` (
+  `id` int(11) NOT NULL,
+  `nim` char(11) NOT NULL,
+  `namamhs` varchar(100) NOT NULL,
+  `jk` enum('L','P') NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `kota` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `foto` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tbl_mhs`
+--
+
+INSERT INTO `tbl_mhs` (`id`, `nim`, `namamhs`, `jk`, `alamat`, `kota`, `email`, `foto`) VALUES
+(3, '20051214070', 'Deviananda Yunitasari', 'P', 'Karangrejo', 'Tulungagung', 'devianada@gmail.com', '488-tumblr_56863efd336f3f46c441ca6f7b9a3264_7655b6');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tbl_mhs`
+--
+ALTER TABLE `tbl_mhs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_mhs`
+--
+ALTER TABLE `tbl_mhs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
